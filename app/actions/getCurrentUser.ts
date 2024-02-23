528c29c7f11cb99b89fec9dfc2b02b1189cd7d43
Shyam Radadiya <shyam.radadiya100@gmail.com>
2024-02-23 21:33:42 +0530

    bug fix

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -10,22 +10,25 @@ export default async function getCurrentUser() {
   try {
     const session = await getSession();
 
-    if (!session?.user?.email) return null;
-
-    const currentUser = await prisma.user.findUnique({
-      where: {
-        email: session?.user?.email as string,
-      },
-    });
-
-    if (!currentUser) return null;
-
-    return {
-      ...currentUser,
-      createdAt: currentUser.createdAt.toISOString(),
-      updatedAt: currentUser.updatedAt.toISOString(),
-      emailVerified: currentUser.emailVerified?.toISOString() || null,
-    };
+    if(session !== null){      
+      
+      if (!session?.user?.email) return null;
+      
+      const currentUser = await prisma.user.findUnique({
+        where: {
+          email: session?.user?.email as string,
+        },
+      });
+      
+      if (!currentUser) return null;
+      
+      return {
+        ...currentUser,
+        createdAt: currentUser.createdAt.toISOString(),
+        updatedAt: currentUser.updatedAt.toISOString(),
+        emailVerified: currentUser.emailVerified?.toISOString() || null,
+      };
+    }
   } catch (error: any) {
     return null;
   }
